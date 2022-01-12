9bf0022863361b9299ab521ad7f5f8d09ad3085f
flavien <flavien_macquignon@fastmail.com>
2022-01-12 14:57:11 +0100

    Sanitized Models

--- src/Quizzator.Models/ThemeEx.cs
+++ src/Quizzator.Models/ThemeEx.cs
@@ -1,8 +1,4 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Text.Json;
-using JsonSerializer = System.Text.Json.JsonSerializer;
+﻿using System.Collections.Generic;
 
 namespace Quizzator.Quizzator.Models
 {
@@ -16,56 +12,13 @@ namespace Quizzator.Quizzator.Models
         {
             _name = name;
             _imagePath = imagePath;
+            // TODO Check si cela ne va pas causer un conflit
             _question = new List<Question>();
         }
 
         public ThemeEx(string name, string imagePath, List<Question> l) : this(name, imagePath)
         {
             _question = l;
-        }
-
-        public void AfficherList()
-        {
-            foreach (Question i in _question)
-            {
-                Console.WriteLine(" +++ " + i);
-            }
-        }
-
-        public void SetName(string name)
-        {
-            _name = name;
-        }
-
-        public void SetImagePath(string imPath)
-        {
-            _imagePath = imPath;
-        }
-
-        public void SetList(List<Question> l)
-        {
-            _question = l;
-        }
-
-        public string SerializingTheme()
-        {
-            string output = JsonSerializer.Serialize(this, new JsonSerializerOptions { WriteIndented = true });
-            return output;
-        }
-
-        public string GetName()
-        {
-            return _name;
-        }
-
-        public string GetImagePath()
-        {
-            return _imagePath;
-        }
-
-        public List<Question> GetListQuestion()
-        {
-            return _question;
         }
 
     }
