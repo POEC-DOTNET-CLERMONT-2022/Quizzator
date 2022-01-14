1604bd594d018e27cc842a266bb361f522801fa0
flavien <flavien_macquignon@fastmail.com>
2022-01-14 12:33:41 +0100

    Added a User class and his DTO

---
+++ src/Quizzator.Dtos/UserDtos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quizzator.Quizzator.Dtos
+{
+    internal class UserDtos
+    {
+        private int _UserId { get; set; }
+        private string _Username { get; set; }
+        private string _PasswordSalted { get; set; }
+        private string _Email { get; set; }
+    }
+}
---
+++ src/Quizzator.Models/User.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Quizzator.Quizzator.Models
+{
+    internal class User
+    {
+        private Guid _UserId { get; set; }
+        private string _Username { get; set; }
+        private string _Salt;
+        private string _PasswordSalted { get; set; }
+        private string _Email { get; set; }
+
+        public User(string UserName, string Password, string Email)
+        {
+            _UserId = new Guid();
+            _Username = UserName;
+            setSalt(GenerateSalt());
+            _PasswordSalted = ComputeHash(Encoding.UTF8.GetBytes(Password), Encoding.UTF8.GetBytes(getSalt()));
+            _Email = Email;
+        }
+        internal bool MatchPassword(string Password)
+        {
+            if (ComputeHash(Encoding.UTF8.GetBytes(Password), Encoding.UTF8.GetBytes(getSalt())) == _PasswordSalted)
+                return true;
+            return false;
+        }
+        internal string ComputeHash(byte[] bytesToHash, byte[] salt)
+        {
+            var byteResult = new Rfc2898DeriveBytes(bytesToHash, salt, 10000);
+            return Convert.ToBase64String(byteResult.GetBytes(24));
+        }
+        internal string GenerateSalt()
+        {
+            var bytes = new byte[128 / 8];
+            var rng = new RNGCryptoServiceProvider();
+            rng.GetBytes(bytes);
+            return Convert.ToBase64String(bytes);
+        }
+        private string getSalt() { return _Salt; }
+        private void setSalt(string value) { _Salt = value; }
+    }
+}
